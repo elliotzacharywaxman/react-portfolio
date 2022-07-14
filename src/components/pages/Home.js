f12087954c7e04fd099512cb061ddefd71cf82f4
elliotzacharywaxman <elliot@climatedao.xyz>
2022-07-14 17:42:45 -0400

    adding images

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,32 @@
 import React from 'react';
 
-const style = {
-  fontSize: '72px',
-  color: "purple"
+const home = {
+  header: {
+    fontSize: '72px',
+    color: "purple"
+  },
+  images: {
+    width: '180px',
+    className: "card-img-top"
+  },
+  notThicc: {
+    width: "300px"
+  }
 }
 
 export default function Home() {
   return (
+
     <div>
-      <h1 style={style}>Home Page</h1>
+      <h1 style={home.header}>Home Page</h1>
+      <div class="card" style={home.notThicc}>
+        <img src="https://images.wagwalkingweb.com/media/care/can-dogs-get-glasses/can-dogs-get-glasses.jpg" style={home.images} alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#contact" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
         velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
